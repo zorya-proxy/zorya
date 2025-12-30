079d4c42b34f62b3713523c784a2a139371a2dcf
juliakam588 <julka.kamuda@gmail.com>
2025-12-30 21:32:31 +0100

    chore: init backend architecture and maven modules

---
+++ backend/zorya-platform/zorya-api/src/main/java/com/zorya/ZoryaApplication.java
@@ -0,0 +1,12 @@
+package com.zorya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoryaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoryaApplication.class, args);
+    }
+}
