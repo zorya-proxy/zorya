d7352ce91ea80263714fcc72d5bffa072a83cd8a
tnt120 <apaj04@gmail.com>
2026-02-12 14:49:25 +0100

    feat: analysis text area component

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,7 +1,30 @@
+import { AnalysisArea } from "@/features/home/components/AnalysisArea";
+import { useState } from "react";
+
 export default function Home() {
+  const [text, setText] = useState<string>("");
+  const [processedText, setProcessedText] = useState<string | null>(null);
+  const [isAnalyzing, setIsAnalyzing] = useState<boolean>(false);
+
+  const handleChange = (value: string) => {
+    setText(value);
+  };
+
+  const handleAnalyze = () => {
+    console.log("Analyzing:", text.trim());
+    setIsAnalyzing(true);
+
+    setTimeout(() => {
+      setIsAnalyzing(false);
+      setProcessedText(`${text}`);
+    }, 1000);
+  };
+
   return (
-    <main className="flex justify-center items-center w-full">
-      <h1>Home Page Zorya Proxy</h1>
+    <main className="flex flex-col justify-center items-center w-full">
+      <div className="flex flex-1 w-full h-screen select-none">
+        <AnalysisArea value={text} onChange={handleChange} onAnalyze={handleAnalyze} isAnalyzing={isAnalyzing} />
+      </div>
     </main>
   );
 }
